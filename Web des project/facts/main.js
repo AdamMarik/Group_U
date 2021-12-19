2af1a1318ab06bea5fcbcc8264e08a6693a67de7
AdamMarik <adamar200@gmail.com>
2021-12-19 14:25:02 +0000

    website

---
+++ Web des project/facts/main.js
@@ -0,0 +1,15 @@
+function goback(){
+	document.getElementById('myImage')
+	.src="../images/FactOff.jpg";
+	document.getElementById('message')
+	.innerHTML="Click for Fun Fact";
+}
+function clickhere(){
+	document.getElementById('myImage')
+	.src="../images/FactOn.jpg";
+	document.getElementById('message')
+	.innerHTML="Hope you like it";
+}
+
+	
+	
